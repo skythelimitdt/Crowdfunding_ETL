9ab4052318eacb4ad75c852a08d98003cb94849b
skythelimitdt <eylemy2020@gmail.com>
2024-12-19 20:37:21 -0700

    updated files

---
+++ crowfunding_db_schema.sql
@@ -0,0 +1,54 @@
+﻿
+
+CREATE TABLE "Category" (
+    "category_id" VARCHAR(4)   NOT NULL,
+    "category" VARCHAR(12)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" VARCHAR(8)   NOT NULL,
+    "subcategory" VARCHAR(17)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(12)   NOT NULL,
+    "last_name" VARCHAR(13)   NOT NULL,
+    "email" VARCHAR(42)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(33)   NOT NULL,
+    "description" VARCHAR(53)   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(10)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(2)   NOT NULL,
+    "currency" VARCHAR(3)   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(4)   NOT NULL,
+    "subcategory_id" VARCHAR(8)   NOT NULL
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
